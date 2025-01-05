93cc2e9bbd37981b34161fe3ef5794c9035bce37
tirtahakimpambudhi <awebthp@gmail.com>
2025-01-05 18:50:13 +0700

    chore: Update setup.sh files

--- .provision/setup.sh
+++ .provision/setup.sh
@@ -16,7 +16,7 @@ create_user() {
     username=$1
     password=$2
 
-    sudo useradd -m -s /bin/bash $username
+    sudo useradd -m -s /bin/bash -d /home/$username $username
     echo "$username:$password" | sudo chpasswd
     sudo usermod -aG sudo $username
 
@@ -29,16 +29,19 @@ create_user "hadoop" "hadoop"
 
 # Create .bashrc file if it does not exist
 if [ ! -f /home/hadoop/.bashrc ]; then
-    sudo cp ~/.bashrc /home/hadoop/.bashrc
     sudo chmod 644 /home/hadoop/.bashrc
     sudo chown hadoop:hadoop /home/hadoop/.bashrc
     log_message ".bashrc created for user 'hadoop'"
 fi
 
 # Generate SSH keys
+sudo mkdir -p /home/hadoop/.ssh
+sudo chmod 700 /home/hadoop/.ssh
+sudo chown hadoop:hadoop -R /home/hadoop
 sudo -u hadoop ssh-keygen -t rsa -b 2048 -N '' -f /home/hadoop/.ssh/id_rsa
+sudo chmod 600 /home/hadoop/.ssh/id_rsa
+sudo chmod 644 /home/hadoop/.ssh/id_rsa.pub
 sudo -u hadoop cat /home/hadoop/.ssh/id_rsa.pub >> /home/hadoop/.ssh/authorized_keys
 sudo chown -R hadoop:hadoop /home/hadoop/.ssh
-sudo chmod 700 /home/hadoop/.ssh
 sudo chmod 600 /home/hadoop/.ssh/authorized_keys
 log_message "SSH keys generated for user 'hadoop'"
