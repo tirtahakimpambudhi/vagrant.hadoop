5e0e5b87f1abb87e72a08cc0bbd0624a4521c958
tirtahakimpambudhi <awebthp@gmail.com>
2025-01-25 18:50:46 +0700

    feat(wordcount): :sparkles: Add feature in file WordMapper.java

---
+++ workspace/wordcount/src/main/java/wordcount/WordMapper.java
@@ -0,0 +1,21 @@
+package wordcount;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class WordMapper extends Mapper<Object, Text, Text, IntWritable> {
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    @Override
+    protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        String[] words = value.toString().split("\\s+");
+        for (String w : words) {
+            word.set(w);
+            context.write(word, one);
+        }
+    }
+}
